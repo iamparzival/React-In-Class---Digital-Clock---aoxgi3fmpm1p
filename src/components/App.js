0c622a4742b64bc5b0187d19b97c409ea8fdc7df
Yash <78196745+iamparzival@users.noreply.github.com>
2022-01-24 13:25:33 +0530

    update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import '../styles/App.css';
 const App = () => {
-
+  let currentTime = new Date().toLocaleString();
+  const [time, setTime] = React.useState(currentTime);
+  setInterval(refreshTime, 1000);
+  
+    function refreshTime() {
+    const newTime = new Date().toLocaleString();
+    setTime(newTime);
+  }
+  
   return (
     <div id="main">
-      <div className="date-time"></div>
+      <div className="date-time">{time}</div>
     </div>
   )
 }
